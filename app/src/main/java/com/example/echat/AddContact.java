0f004774ceaaf468293d64363e3ab936832305d4
EdenHamami <92511329+EdenHamami@users.noreply.github.com>
2022-06-12 20:44:21 +0300

    zkk

--- app/src/main/java/com/example/echat/AddContact.java
+++ app/src/main/java/com/example/echat/AddContact.java
@@ -5,7 +5,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
 public class AddContact extends AppCompatActivity {
     private AppDB db;
@@ -15,16 +14,18 @@ public class AddContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
-        db = Room.databaseBuilder(getApplicationContext(), AppDB.class, "ContactsDB").
-                allowMainThreadQueries().build();
+//        db = Room.databaseBuilder(getApplicationContext(), AppDB.class, "ContactsDB").
+//                allowMainThreadQueries().build();
+        db=AppDB.getInstance(AddContact.this);
+
         contactDao = db.contactDao();
         Button saveBtn = findViewById(R.id.saveBtn);
-        saveBtn.setOnClickListener(view -> {
+        saveBtn.setOnClickListener( view -> {
             EditText etIten=findViewById(R.id.etItem);
             String newContactName=etIten.getText().toString();
 //            List<Message> messages=new ArrayList<Message>() {};
             Contact contact=new Contact(0,newContactName,"","","");
-        contactDao.insert(contact);
+            contactDao.insert(contact);
             finish();
         });
     }

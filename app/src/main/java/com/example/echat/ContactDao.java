6b07fb211f58493784556789e6a7e3ac5e3439d2
elaganot <ganot.ela@gmail.com>
2022-06-12 14:39:33 +0300

    c

--- app/src/main/java/com/example/echat/ContactDao.java
+++ app/src/main/java/com/example/echat/ContactDao.java
@@ -8,11 +8,13 @@ import androidx.room.Update;
 
 import java.util.List;
 
+
 @Dao
 public interface ContactDao {
 
     @Query("SELECT * FROM contact")
-    List<Contact> index();
+    List<Contact
+            > index();
 
     @Query("SELECT * FROM contact WHERE id = :id")
     Contact get(int id);

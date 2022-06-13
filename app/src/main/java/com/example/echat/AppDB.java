fa873a8f5dbf6fbd5258b91c6ac4d1c1c279a482
elaganot <ganot.ela@gmail.com>
2022-06-13 22:40:51 +0300

    c

--- app/src/main/java/com/example/echat/AppDB.java
+++ app/src/main/java/com/example/echat/AppDB.java
@@ -8,7 +8,7 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 
-@Database(entities ={Contact.class} ,version = 1)
+@Database(entities ={Contact.class} ,version = 1, exportSchema = false)
 public abstract class AppDB extends RoomDatabase {
     public abstract ContactDao contactDao();
     private static AppDB appDB;
